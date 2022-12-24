64c8ce67f8e20dc30729d0c3a0e5742db856d688
Yurez23 <yurezzz2012@gmail.com>
2022-12-25 01:43:39 +0300

    10001st prime

---
+++ Primes10001.hs
@@ -0,0 +1,16 @@
+{-
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+-}
+import Data.Array
+
+nthPrime :: Int -> Integer
+nthPrime n = primes !! (n - 1)
+  where
+    primes = 2 : filter isPrime [3, 5 ..]
+    isPrime n = all (\x -> n `mod` x /= 0) $ takeWhile (\x -> x * x <= n) primes
+
+answer :: Integer
+answer = nthPrime 10001
